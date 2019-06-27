ae9c733d959af7c84c33f8a6b1d21eba973fc11f
Hyun O Sung <sierrahotel555@gmail.com>
2019-06-27 18:03:28 +0900

    190627 수업

--- JJAMTONG/JJAMTONG/main.cpp
+++ JJAMTONG/JJAMTONG/main.cpp
@@ -6,22 +6,43 @@ using namespace std;
 
 int main()
 {
-	int a[5];
-	int b = 0;
+	int a[10];
+	int b[10];
+	bool q = false;
+	bool w = false;
+	bool e = false;
 
-	for (int i = 0; i < 5; ++i)
-	{		
-		cin >> b;		
-		a[i] = b;
-		printf("%d\n", a[i]);
-	}
-	for (int i = 4; i >= 0; --i)
+	int z = 0;
+
+
+	for (int i = 0; i < 10; ++i)
 	{
-		printf("%d\n", a[i]);
+		cin >> a[i];
+		b[i] = 0;
 	}
-//	printf("%d\n", &a);
+	for (int j = 0; j < 10; j++)
+	{
+		for (int k = 0; k < j; k++)
+		{
+			if (a[k] == a[j])
+			{
+				q = true;
+				break;
+			}
 
+		}
+		if (q == true)
+		{
+			b[z] = a[j];
+			z++;
+		}
 
+	}
 
+	for (int l = 0; l<z; l++)
+	{
+		cout << b[l] << endl;
+
+	}
 	return 0;
 }
