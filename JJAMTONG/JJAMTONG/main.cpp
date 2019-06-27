5fd9e10b5bd84a41590d3aaed2492187c6584f09
Hyun O Sung <sierrahotel555@gmail.com>
2019-06-27 23:44:30 +0900

    190627 집

--- JJAMTONG/JJAMTONG/main.cpp
+++ JJAMTONG/JJAMTONG/main.cpp
@@ -6,43 +6,16 @@ using namespace std;
 
 int main()
 {
-	int a[10];
-	int b[10];
-	bool q = false;
-	bool w = false;
-	bool e = false;
+	int q = 1;
+	int *w = &q;
 
-	int z = 0;
+	cout << q << endl;
+	cout << w << endl;
+	cout << &q << endl;
+	cout << *w << endl;
 
 
-	for (int i = 0; i < 10; ++i)
-	{
-		cin >> a[i];
-		b[i] = 0;
-	}
-	for (int j = 0; j < 10; j++)
-	{
-		for (int k = 0; k < j; k++)
-		{
-			if (a[k] == a[j])
-			{
-				q = true;
-				break;
-			}
 
-		}
-		if (q == true)
-		{
-			b[z] = a[j];
-			z++;
-		}
 
-	}
-
-	for (int l = 0; l<z; l++)
-	{
-		cout << b[l] << endl;
-
-	}
 	return 0;
 }
