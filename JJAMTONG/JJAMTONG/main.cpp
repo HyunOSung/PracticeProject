c2a43763f13895c880b59e52950d89fbed0053ae
Hyun O Sung <sierrahotel555@gmail.com>
2019-06-28 16:15:11 +0900

    190628 수업

--- JJAMTONG/JJAMTONG/main.cpp
+++ JJAMTONG/JJAMTONG/main.cpp
@@ -4,18 +4,57 @@
 
 using namespace std;
 
+
+void Swap(int& a, int& b)
+{
+	int Temp = a;
+	a = b;
+	b = Temp;
+}
+
+void SelectionSort(int* Data, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = i; j < size; ++j)
+		{
+			if (Data[i] > Data[j])
+			{
+				Swap(Data[i], Data[j]);
+			}
+		}
+	}
+}
+
+void BubbleSort(int* Data, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = i; j < size; ++j)
+		{
+			if (Data[i] > Data[j])
+			{
+				Swap(Data[i], Data[j]);
+			}
+		}
+	}
+}
+
 int main()
 {
-	int q = 1;
-	int *w = &q;
+	int Data[10] = { 10, 2, 12, 15, 16, 20, 49, 60, 3, 6 };
 
-	cout << q << endl;
-	cout << w << endl;
-	cout << &q << endl;
-	cout << *w << endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << Data[i] << ", ";
+	}
+	cout << endl;
 
+	SelectionSort(Data, 10);
 
+	for (int i = 0; i < 10; ++i)
+	{
+		cout << Data[i] << ", ";
+	}
+	cout << endl;
 
-
-	return 0;
-}
