99711a6d97d0b45525261d1b6f13b00626e0e726
Hyun O Sung <sierrahotel555@gmail.com>
2019-07-01 00:52:54 +0900

    190701 새벽업로드

--- Test00/Test00/main.cpp
+++ Test00/Test00/main.cpp
@@ -31,6 +31,11 @@ bool escape = false;
 bool stuck = false;
 
 
+const int QueueSize = 111; //배열 범위 설정
+int Data[QueueSize][2] = {}; // 루트 추적할 배열 설정
+int pY = 0; //배열 저장용 Y 좌표
+
+
 double MapData[Size][Size] = {
 	{ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
 	{ 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1 },
@@ -54,16 +59,6 @@ double MapData[Size][Size] = {
 	{ 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0 }
 };
 
-
-//enum class Move
-//{
-//	//배열 탐색할 때 이동할 방향 설정
-//	LEFT = 0,
-//	UP = 1,
-//	RIGHT = 2,
-//	DOWN = 3
-//};
-
 int PointSetting(int Object, int y, int x)
 {
 	//시작, 탈출, 이외 기타 오브젝트 셋팅용 함수
@@ -76,10 +71,6 @@ int PointSetting(int Object, int y, int x)
 	return  0;
 }
 
-
-
-
-
 bool SearchPath(int x, int y)
 {
 	if (x<0 || y<0 || x>Size-1 || y>Size-1)
@@ -91,6 +82,12 @@ bool SearchPath(int x, int y)
 	{
 		//탈출점 도착하면 탈출
 		MapData[y][x] = GOAL;
+		Data[pY][0] = { y };
+		Data[pY][1] = { x };
+
+		escape = true;
+		
+		//cout << Data[pY][0] << " " << Data[pY][1] << endl;
 		return true;
 	}
 	else if (MapData[y][x] != WAY)
@@ -106,11 +103,12 @@ bool SearchPath(int x, int y)
 			SearchPath(x, y+1) ||
 			SearchPath(x, y-1) ||
 			SearchPath(x+1, y))
-		//if (SearchPath(x - 1, y) ||
-		//	SearchPath(x, y - 1) ||
-		//	SearchPath(x + 1, y) ||
-		//	SearchPath(x, y + 1))
 		{
+			Data[pY][0] = { y };
+			Data[pY][1] = { x };
+
+			//cout << Data[pY][0] << endl;
+			++pY;
 			return true;
 		}
 		else
@@ -141,23 +139,27 @@ int PrintOutEscapeRoute()
 {
 	cout << "------------탈출 경로-------------" << endl;
 	//탈출경로 인쇄
-	
-	for (int i = 0; i < Size; ++i)
+	for (int i = 111; i < 0; --i)
 	{
-		for (int j = 0; j < Size; ++j)
-		{
-			if (MapData[i][j] == ROUTE)
-			{				
-				cout << "{" << i << " , " << j << "}" << endl;
-			}
-			else if (MapData[i][j] == GOAL)
-			{
-				cout << "{" << i << " , " << j << "}" << "탈출지점입니다. " << endl;
-				break;
-			}
-		}
+		cout << "반복문진입" << endl;
+		cout << Data[i][0] << " " << Data[i][1] << endl;
 	}
 
+	//for (int i = 0; i < Size; ++i)
+	//{
+	//	for (int j = 0; j < Size; ++j)
+	//	{
+	//		if (MapData[i][j] == ROUTE)
+	//		{				
+	//			cout << "{" << i << " , " << j << "}" << endl;
+	//		}
+	//		else if (MapData[i][j] == GOAL)
+	//		{
+	//			cout << "{" << i << " , " << j << "}" << "탈출지점입니다. " << endl;
+	//			break;
+	//		}
+	//	}
+	//}
 	return  0;
 }
 
@@ -173,12 +175,16 @@ int main()
 	int escapeX = 19;
 	int escapeY = 19;
 
+	//cout << Data[0][0] << "," << Data[0][3] << endl;
+
 	PointSetting(rand()%4,startingX ,startingY ); //시작지점 설정
 	PointSetting(END, escapeY, escapeX);  //탈출지점 설정
+
+
 
 	SearchPath(startingY, startingX);
 	PrintMap();
 	PrintOutEscapeRoute();
 	
-	
+
 }
